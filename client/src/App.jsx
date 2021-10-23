cf1fe2b781d605ac0fff7f99c384fe1ce9fd22f4
saifulshihab <shihabmd1970@gmail.comm>
2021-10-23 15:04:49 +0600

    add limit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
 import { gql, useQuery } from "@apollo/client";
+import { useState } from "react";
 import Loader from "./components/Loader";
 
 const GET_AXIE_BRIEF_LIST = gql`
-  query GetAxieBriefList {
-    axies {
+  query GetAxieBriefList(
+    $auctionType: AuctionType
+    $criteria: AxieSearchCriteria
+    $from: Int
+    $sort: SortBy
+    $size: Int
+    $owner: String
+  ) {
+    axies(
+      auctionType: $auctionType
+      criteria: $criteria
+      from: $from
+      sort: $sort
+      size: $size
+      owner: $owner
+    ) {
       results {
         id
         title
@@ -20,27 +35,45 @@ const GET_AXIE_BRIEF_LIST = gql`
   }
 `;
 function App() {
-  const { loading, error, data } = useQuery(GET_AXIE_BRIEF_LIST);
+  const [limit, setLimit] = useState(10);
+
+  const { loading, error, data } = useQuery(GET_AXIE_BRIEF_LIST, {
+    variables: { auctionType: "Sale", from: 0, size: limit, sort: "PriceAsc" },
+  });
   return (
-    <div class="container">
-      <div class="jumbotron">
-        <h1 class="display-4">Axies API</h1>
-        <p class="lead">Data from Axies</p>
-        <hr class="my-4" />
+    <div className="container">
+      <div className="jumbotron">
+        <h1 className="display-4">Axies API</h1>
+        <p className="lead">Data from Axies</p>
+        <hr className="my-4" />
         <div>
+          <div className="d-flex justify-content-between">
+            <p className="fw-bold">Showing {limit} results</p>
+            <select
+              value={limit}
+              onChange={(e) => setLimit(parseInt(e.target.value))}
+              name="limitSelect"
+              id="limitSelect"
+            >
+              <option value={10}>10</option>
+              <option value={20}>20</option>
+              <option value={50}>50</option>
+              <option value={100}>100</option>
+            </select>
+          </div>
           {loading ? (
             <Loader />
           ) : error ? (
             "Failed to load data.."
           ) : data && data.axies?.results?.length > 0 ? (
-            <table class="table table-striped">
+            <table className="table table-striped">
               <thead>
                 <tr>
                   <th scope="col">ID</th>
                   <th scope="col">Title</th>
                   <th scope="col">Name</th>
                   <th scope="col">Stage</th>
-                  <th scope="col">Class</th>
+                  <th scope="col">className</th>
                   <th scope="col">Breed Count</th>
                   <th scope="col">Current Price</th>
                   <th scope="col">Current Price USD</th>
@@ -53,7 +86,7 @@ function App() {
                     <td>{item.title ? item.title : "-"}</td>
                     <td>{item.name}</td>
                     <td>{item.stage}</td>
-                    <td>{item.class}</td>
+                    <td>{item.className}</td>
                     <td>{item.breedCount}</td>
                     <td>
                       {item.auction?.currentPrice
